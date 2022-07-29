822dc07536d7d36f34d2ee72fe3cef36719df46a
Kevin Renato <kev.renato@gmail.com>
2022-07-28 22:24:59 -0400

    Fixed loading config.

--- WordSolverAng.Api/Services/ConfigService.cs
+++ WordSolverAng.Api/Services/ConfigService.cs
@@ -8,9 +8,9 @@ namespace WordSolverAng.Api.Services
     {
         public ConfigService(IConfiguration config, IWordSolverService wordSolverService)
         {
-            if (int.TryParse(config[ConfigValues.NumberOfGuesses], out int numberOfGuesses))
+            if (!int.TryParse(config[ConfigValues.NumberOfGuesses], out int numberOfGuesses))
                 throw new FormatException($"Unable to parse {ConfigValues.NumberOfGuesses} value from configuration.");
-            if (int.TryParse(config[ConfigValues.WordLength], out int wordLength))
+            if (!int.TryParse(config[ConfigValues.WordLength], out int wordLength))
                 throw new FormatException($"Unable to parse {ConfigValues.WordLength} value from configuration.");
             if (string.IsNullOrEmpty(config[ConfigValues.WordFilePath]))
                 throw new FormatException($"Unable to parse {ConfigValues.WordFilePath} value from configuration.");
